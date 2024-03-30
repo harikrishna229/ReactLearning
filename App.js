c2d9acfaa745ae620426c1515db670b419782267
harikrishna229 <yeturuharikrishna@gmail.com>
2024-03-30 13:28:49 +0530

    episode3

--- App.js
+++ App.js
@@ -1,27 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//jsx- babel will convert jsx to react.createelemnt
+// jsx is not html similar like syntax
+// write attributes in camel case
+ const head=(<h1 className="head">hello im 1 st element</h1>); // React element
+
+ const head2= <h1 className="head">{head}hello im 2 nd element</h1>; // React element
+
+ // React Functional component
+ const Title= () => (
+    <div>
+        {head2}
+        <h1>heloo im functional component Title</h1>
+    </div>
+ 
+ );
+
+ // React Functional component
+
+ const HeadingComponent= () => (
+    <div id="container">
+            <Title/>
+        <h1>heloo im functional heading component</h1>
+    </div>
+ 
+ );
 
 
-
-
-var parent=React.createElement(
-    "div",
-    {id: "parent"},[
-    React.createElement("div",{id:"child1"},
-    [React.createElement("h1",{},"im h1 tag "),React.createElement("h2",{},"im h2 tag ")]
-    ),
-    React.createElement("div",{id:"child2"},
-    [React.createElement("h1",{},"im h1 tag "),React.createElement("h2",{},"im h2 tag ")]
-    )
-    ]
-);
-
-// const heading =React.createElement("h1",
-// {id:"child",xyx:"abcd"},
-// "Hello world from React");
-console.log(parent) // it is a object
+ //<Title></Title>     below 3 ways are same
+ //<Title/>
+//  {Title()}
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// root.render(head);
+root.render(<HeadingComponent/>); // waay of rendering component
 
